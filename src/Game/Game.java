5d5c468623e691b694e1ed3e71e726d43ef8cb03
dab1231 <nikita11200701@gmail.com>
2025-11-07 13:39:46 +0400

    обработка слов и ход

---
+++ src/Game/Game.java
@@ -0,0 +1,56 @@
+package Game;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+import generate.*;
+public class Game {
+    public char[] word;
+    public char[] guess;
+    public int steps = 6;
+
+    public Game() throws IOException {
+        word = getChars(Generate.GenerateWord());
+        guess = makeGuess();
+    }
+    
+    public char[] getChars(String word) {
+        this.word = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            this.word[i] = word.charAt(i);
+        }
+        return this.word;
+    }
+
+    public char[] makeGuess() {
+        this.guess = new char[word.length];
+        Random random = new Random();
+        for(int i = 0; i < guess.length; i++){
+            guess[i] = '_';
+        }
+        int index = random.nextInt(word.length);
+        int index2 = random.nextInt(word.length);
+        guess[index] = word[index];
+        guess[index2] = word[index2];
+        return this.guess;
+    }
+
+    public void Step(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите букву: ");
+        char letter = scanner.next().charAt(0);
+        
+        boolean found = false;
+        for(int i = 0; i < word.length; i++){
+            if(word[i] == letter){
+                guess[i] = letter;
+                found = true;
+            }
+        }
+        if(found){
+            System.out.println("Есть такая буква!");
+        } else {
+            System.out.println("Такой буквы нет.");
+        }
+    }
+}
---
+++ src/generate/Generate.java
@@ -0,0 +1,29 @@
+package generate;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.Random;
+
+public class Generate {
+    public static String GenerateWord() throws IOException {
+        
+        FileReader fileReader = new FileReader("russian-mnemonic.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        int lim = 33740;
+        Random random = new Random();
+        int rand = random.nextInt(lim);
+        int current = 1;
+
+        String line = bufferedReader.readLine();
+        while(current < rand) {
+            line = bufferedReader.readLine();
+            current++;
+        }
+        
+        bufferedReader.close();
+        return line;
+    }
+}
