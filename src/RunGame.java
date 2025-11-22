953a8e2bb879567ca0516a35d34b9f316a2fcf95
dab1231 <nikita11200701@gmail.com>
2025-11-22 21:51:52 +0400

    доломал меню end

--- src/RunGame.java
+++ src/RunGame.java
@@ -5,22 +5,30 @@ import Game.Game;
 import Game.Utils;
 
 public class RunGame {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         boolean isGameRunning = true;
         Scanner scanner = new Scanner(System.in);
         while (isGameRunning) {
             System.out.println("Выберите пункт меню:");
             System.out.println("1. Виселица");
             System.out.println("2. Выход");
-            int point = scanner.nextInt();
-            switch (point) {
-            case 1:
-                Game game = new Game();
-                game.startGame();
-                break;
-            case 2:
-                System.exit(0);
-         }
+
+            if (scanner.hasNextInt()) {
+                int point = scanner.nextInt();
+                switch (point) {
+                    case 1:
+                        Game game = new Game();
+                        game.startGame();
+                        break;
+                    case 2:
+                        System.exit(0);
+                    default:
+                        System.out.println("\nВведите 1 или 2\n");
+                }
+            } else {
+                System.out.println("\nОшибка: введите число\n");
+                scanner.next(); 
+            }
         }
         scanner.close();
     }
